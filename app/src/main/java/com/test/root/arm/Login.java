ae7e53e2f24a821efb4ba5e0bb37b2dea077718c
shiva kishore <shivakishore14@gmail.com>
2016-04-24 22:44:51 +0530

    Modified project | native JDBC connection removed from app

--- app/src/main/java/com/test/root/arm/Login.java
+++ app/src/main/java/com/test/root/arm/Login.java
@@ -26,7 +26,7 @@ public class Login extends Activity{
     EditText uname, pass;
     TextView notification;
     SharedPreferences sp ;
-    String dbname,dbpass,dbip;
+
     Boolean a;
     ProgressDialog progress ;
 
@@ -39,9 +39,6 @@ public class Login extends Activity{
         login = (Button)findViewById(R.id.btlogin);
         notification = (TextView) findViewById(R.id.tvLoginNotification);
         sp  = getApplicationContext().getSharedPreferences("dbinfo", Context.MODE_PRIVATE);
-        dbname = sp.getString("dbuname", null);
-        dbpass = sp.getString("dbpass", null);
-        dbip = sp.getString("dbip",null);
         progress = new ProgressDialog(this);
         progress.setMessage("Loging in...");
 
@@ -53,10 +50,6 @@ public class Login extends Activity{
             getWindow().setStatusBarColor(Color.parseColor("#00796B"));
         }
 
-        if(dbname == null || dbpass == null || dbip == null){
-            Intent i = new Intent(Login.this,Settings.class);
-            startActivity(i);
-        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,57 +57,11 @@ public class Login extends Activity{
                 String password = pass.getText().toString();
                 if (username.length() != 0) {
                     notification.setText("Logging in as :" + username);
-                    BackTask st = new BackTask();
-                    st.execute("select username from login where username like '" + username + "' and password like '" + password + "'");
-                } else {
+                   } else {
                     notification.setText("Enter valid username");
                 }
             }
         });
 
-    }
-    private class BackTask extends AsyncTask<String, String, String> {
-
-        private String result;
-
-        @Override
-        protected String doInBackground(String... params) {
-            //start
-            util obj = new util("192.168.1.10:3306","test","test");
-            try {
-                ResultSet a = util.getResult("armtest",params[0]);
-                if (a.next()){
-                    return a.getString("username");
-                }
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            progress.dismiss();
-            String name = uname.getText().toString();
-            if(result == null || name == null){
-                //Toast.makeText(getBaseContext(),"Invalid Username or Password",Toast.LENGTH_SHORT).show();
-                notification.setText("Invalid Username or Password");
-            }else if (result.equals(name)){
-                Intent i = new Intent(Login.this,Selection.class);
-                startActivity(i);
-            }else {
-                notification.setText("Please try again");
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-            progress.show();
-        }
-
-        @Override
-        protected void onProgressUpdate(String... text) {
-            // progress. For example updating ProgessDialog
-        }
     }
 }
